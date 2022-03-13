dc07eed407db1c5d8a09b35bfdfdb610a7d5d623
Sami Eskirjeh <32425558+SamiESK@users.noreply.github.com>
2022-03-13 17:13:03 -0400

    Fixing prisma schema dir/more funct
    Removed Prisma folder from main directory of branch feature/users and added some more CRUD functionality.

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,25 +15,34 @@ router.get('/', async (req, res) => {
 })
 
 //Post user info to local server 3306
-// router.post('/', async (req, res) => {
-//     const { name } = req.body;
+router.post('/', async (req, res) => {
+    const { name } = req.body;
+    const { email } = req.body;
 
-//     const userExists = await user.findUnique({
-//        where: { 
-//             name 
-//         },
-//         select: {
-//             name: true
-//         }
-//     });
+    const userExists = await user.findUnique({
+       where: { 
+            name 
+        },
+        select: {
+            name: true
+        }
+    });
 
-//Check if user already exists in db
-//     if(userExists) {
-//         return res.status(400).json({
-//             msg: "User already exists"
-//         })
-//     }
+// Check if user already exists in db
+    if(userExists) {
+        return res.status(400).json({
+            msg: "User already exists"
+        })
+    }
 
-// })
+//Create new user 
+    const newUser = await user.create({
+        data: {
+            name,
+            email
+        }
+    });
+    res.json(newUser)
+})
 
 module.exports = router
