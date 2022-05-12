a6e0cc9628977744667b5c42fc7477fd6ba867c9
Samdurp <noahpparentreason@gmail.com>
2022-05-11 21:13:09 -0400

    Thanks Quan

--- frontend/src/Pages/Test.js
+++ frontend/src/Pages/Test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import Cards from '../Components/Cards/Cards'
 
@@ -12,9 +12,92 @@ import * as datastreams from "https://cdn.jsdelivr.net/npm/datastreams-api@lates
 import ganglion from "https://cdn.jsdelivr.net/npm/@brainsatplay/ganglion@0.0.2/dist/index.esm.js";
 
 const Test = () => {
+
+  useEffect(()=>{const dataDevices = new datastreams.DataDevices();
+    dataDevices.load(ganglion);
+    
+    // ------------- Setup Visualization (very rough) -------------
+    const graphDiv = document.getElementById("graph");
+    graphDiv.style.padding = "25px";
+    const timeseries = new components.streams.data.TimeSeries();
+    graphDiv.insertAdjacentElement("beforeend", timeseries);
+    
+    // ------------- Declare Global Variables -------------
+    const allData = [];
+    let channels = 0;
+    let trackMap = new Map();
+    let contentHintToIndex = {};
+    
+    // ------------- Declare Data Handler -------------
+    const ondata = (data, timestamps, contentHint) => {
+        console.log(
+        `Data from Electrode ${contentHintToIndex[contentHint]} (${contentHint})`,
+        data,
+        timestamps
+        );
+    };
+    
+    {/* // ------------- Declare Track Handler ------------- */}
+    const handleTrack = (track) => {
+        // ------------- Map Track Information (e.g. 10-20 Coordinate) to Index -------------
+        if (!trackMap.has(track.contentHint)) {
+        const index = trackMap.size;
+        contentHintToIndex[track.contentHint] = index;
+        trackMap.set(index, track);
+        }
+    
+        // ------------- Grab Index -------------
+        const i = contentHintToIndex[track.contentHint];
+        channels = i > channels ? i : channels; // Assign channels as max track number
+    
+        // ------------- Subscribe to New Data -------------
+        track.subscribe((data, timestamps) => {
+        // Organize New Data
+        let arr = [];
+        for (let j = 0; j <= channels; j++)
+            i === j ? arr.push(data) : arr.push([]);
+    
+        // Add Data to Timeseries Graph
+        timeseries.data = arr;
+        //debugger;
+        //console.log(i, arr);
+        timeseries.draw(); // FORCE DRAW: Update happens too fast for UI
+    
+        // Run ondata Callback
+        ondata(data, timestamps, track.contentHint);
+        });
+    };
+    
+    // // ------------- Declare Acquisition Function -------------
+    
+    const startAcquisition = async (label) => {
+        // ------------- Get Device Stream -------------
+    
+        // Method #1: By Label
+        const dataDevice = await dataDevices.getUserDevice({ label });
+    
+        // Method #2: By Class
+        // const dataDevice = await dataDevices.getUserDevice(ganglion)
+    
+        // ------------- Grab DataStream from Device -------------
+        const stream = dataDevice.stream;
+    
+        // ------------- Handle All Tracks -------------
+        stream.tracks.forEach(handleTrack);
+        stream.onaddtrack = (e) => handleTrack(e.track);
+    };
+    
+    // ------------- Set Button Functionality -------------
+    
+    const buttons = document.querySelector("#buttons");
+    
+    console.log(buttons);
+    
+    for (let button of buttons.querySelectorAll("button"))
+        button.onclick = () => startAcquisition(button.id);})
+
   return (
     <>
-   <body style="padding: 10px">
     <h1>TEST</h1>
     <hr />
     <br />
@@ -23,92 +106,8 @@ const Test = () => {
       <button id="ganglion">Connect</button>
     </div>
     <div id="graph"></div>
-  </body>
     </>
   )
 }
-
-const dataDevices = new datastreams.DataDevices();
-dataDevices.load(ganglion);
-
-// ------------- Setup Visualization (very rough) -------------
-const graphDiv = document.getElementById("graph");
-graphDiv.style.padding = "25px";
-const timeseries = new components.streams.data.TimeSeries();
-graphDiv.insertAdjacentElement("beforeend", timeseries);
-
-// ------------- Declare Global Variables -------------
-const allData = [];
-let channels = 0;
-let trackMap = new Map();
-let contentHintToIndex = {};
-
-// ------------- Declare Data Handler -------------
-const ondata = (data, timestamps, contentHint) => {
-    console.log(
-    `Data from Electrode ${contentHintToIndex[contentHint]} (${contentHint})`,
-    data,
-    timestamps
-    );
-};
-
-{/* // ------------- Declare Track Handler ------------- */}
-const handleTrack = (track) => {
-    // ------------- Map Track Information (e.g. 10-20 Coordinate) to Index -------------
-    if (!trackMap.has(track.contentHint)) {
-    const index = trackMap.size;
-    contentHintToIndex[track.contentHint] = index;
-    trackMap.set(index, track);
-    }
-
-    // ------------- Grab Index -------------
-    const i = contentHintToIndex[track.contentHint];
-    channels = i > channels ? i : channels; // Assign channels as max track number
-
-    // ------------- Subscribe to New Data -------------
-    track.subscribe((data, timestamps) => {
-    // Organize New Data
-    let arr = [];
-    for (let j = 0; j <= channels; j++)
-        i === j ? arr.push(data) : arr.push([]);
-
-    // Add Data to Timeseries Graph
-    timeseries.data = arr;
-    //debugger;
-    //console.log(i, arr);
-    timeseries.draw(); // FORCE DRAW: Update happens too fast for UI
-
-    // Run ondata Callback
-    ondata(data, timestamps, track.contentHint);
-    });
-};
-
-// // ------------- Declare Acquisition Function -------------
-
-const startAcquisition = async (label) => {
-    // ------------- Get Device Stream -------------
-
-    // Method #1: By Label
-    const dataDevice = await dataDevices.getUserDevice({ label });
-
-    // Method #2: By Class
-    // const dataDevice = await dataDevices.getUserDevice(ganglion)
-
-    // ------------- Grab DataStream from Device -------------
-    const stream = dataDevice.stream;
-
-    // ------------- Handle All Tracks -------------
-    stream.tracks.forEach(handleTrack);
-    stream.onaddtrack = (e) => handleTrack(e.track);
-};
-
-// ------------- Set Button Functionality -------------
-
-const buttons = document.querySelector("#buttons");
-
-console.log(buttons);
-
-for (let button of buttons.querySelectorAll("#button"))
-    button.onclick = () => startAcquisition(button.id);
 
 export default Test
