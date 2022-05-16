b88b1f2b425b09e9760d5795baeda071f058cd52
Quanminh Nguyen <quanminh@Quanminhs-Air.attlocal.net>
2022-05-16 19:35:31 -0400

    recording page outline andanimated recording button

--- frontend/src/Components/Record/Record.js
+++ frontend/src/Components/Record/Record.js
@@ -1,13 +1,10 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import RecordButton from './RecordButton';
 
 function Record() {
-
     //Set onLoad to link
     const [type, setType] = useState('link');
 
-    function clicky() {
-        alert(type)
-    }
     return (
         <div>
             <select onChange={e => { setType(e.target.value) }}>
@@ -16,6 +13,7 @@ function Record() {
             </select>
             <ScriptThing show={type === "script"} />
             <LinkThing show={type === "link"} />
+            <Setting />
         </div>
     );
 }
@@ -27,9 +25,6 @@ function LinkThing(shown) {
     const [id, setId] = useState('');
     let correctLink = id.split('=');
 
-    function handelChange(e) {
-
-    }
     if (shown.show) {
         return (
             <div>
@@ -53,6 +48,63 @@ function ScriptThing(shown) {
         return <div>Script Stuff Here</div>
     }
     return <></>;
+}
+
+function Setting() {
+    let st = {
+        height: '100px',
+        position: 'fixed',
+        bottom: '0%',
+        width: '100%',
+        backgroundColor: '#393838',
+        opacity: '1',
+    }
+
+    return (
+        <>
+            <div style={st}>
+                <table style={{width: '100%', height: '100px' }}>
+                    <tr>
+                        <td style={{ width: '25%',textAlign: 'center'}}>
+                            <div style={{color: 'white'}}>Key Signature</div>
+
+                            <select>
+                                <option>C</option>
+                                <option>C#/Db</option>
+                                <option>D</option>
+                                <option>D#/Eb</option>
+                                <option>E</option>
+                                <option>F</option>
+                                <option>F#/Gb</option>
+                                <option>G</option>
+                                <option>G#/Ab</option>
+                                <option>A</option>
+                                <option>B</option>
+                            </select>
+                            <div style={{color: 'white'}}>Scale</div>
+                            <select>
+                                <option>Major</option>
+                                <option>Minor</option>
+                            </select>
+                        </td>
+                        <td style={{width: '22%', textAlign: 'left'}}>
+                            
+                        </td>
+                        <td>
+                            <RecordButton />
+                        </td>
+                        <td style={{width: '25%', textAlign: 'center'}}>
+                            <div style={{color: 'white'}}>Tempo</div>
+                            <input type="text" defaultValue="120" />
+                        </td>
+                    </tr>
+
+                </table>
+            </div>
+
+        </>
+    );
+
 }
 
 export default Record;
