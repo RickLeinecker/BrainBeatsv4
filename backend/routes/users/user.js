e6a60150a1c67f384308e2c7402943d63e101baa
Sami Eskirjeh <shomee@live.com>
2022-06-12 02:46:07 -0400

    Removed files since users folder was too modularized. Cleaned users folder, and added all requests in a user.js file.
    Eased the readability of code but keeping one file for all requests and removing the rest.

---
+++ backend/routes/users/user.js
@@ -0,0 +1,372 @@
+require("dotenv").config();
+const bcrypt =  require('bcryptjs');
+const jwt =  require('jsonwebtoken');
+const router = require("express").Router();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { user, post } = new PrismaClient();
+var nodemailer = require("nodemailer");
+const jwtAPI = require("../../utils/jwt");
+
+
+// Create a new user
+router.post('/createUser', async (req, res) => {
+
+    try 
+    {
+        const { firstName, lastName, dob, email, username, password, bio } = req.body;
+
+        // Check if the email already exists in db
+        const userEmailExists = await prisma.user.findUnique({
+        where: { email },
+        });
+
+        // Check if the username already exists in db
+        const userNameExists = await prisma.user.findUnique({
+        where: { username },
+        });
+
+        if(userEmailExists || userNameExists) {
+            return res.status(400).json({
+                msg: "Email or username already exists. Please try again."
+            })
+        } else {
+            
+            //Encrypt user password
+            encryptedPassword = await bcrypt.hash(password, 10);
+
+            //Create a single record
+            const newUser = await prisma.user.create({
+                data: {
+                    firstName,
+                    lastName,
+                    dob,
+                    email,
+                    username,
+                    password: encryptedPassword,
+                    bio
+                }
+            });
+
+            // Create token
+
+            // const token = jwtAPI.giveSignUpJWT(newUser.id, newUser.email);
+
+            //----COMMENTED OUT TO CLOSE USER LOOP----\\ 
+            // const token = jwt.sign(
+            //     { userId: newUser.id, email },
+            //     process.env.NEXT_PUBLIC_JWT_KEY,
+            //     {
+            //         expiresIn: "1h",
+            //     }
+            //     );
+
+            // // save user token
+            // newUser.token = token;
+
+            res.json(newUser);
+        }
+
+    } 
+    catch(err) {
+        console.log(err)
+        res.status(500).json({msg: "Unable to create user"})
+    }
+
+});
+
+//Get all users with all records
+router.get('/getAllUsers', async (req, res) => {
+
+    try 
+    {
+        const users =  await prisma.user.findMany({
+            select: {
+                id: true,
+                firstName: true,
+                lastName: true,
+                dob: true,
+                email: true,
+                username: true,
+                password: true,
+                bio: true,
+                posts: true
+            }
+        });
+        res.json(users)
+    } 
+    catch(err) {
+        res.status(500).send({msg: err})
+    }
+    
+});
+
+// Post at user account
+router.post('/createPost', async (req, res) => {
+
+    try 
+    {
+        const { id, title, bpm, key} =  req.body
+        const userExists = await prisma.user.findUnique({
+            where: { id },
+        });
+
+        if(!userExists) {
+            return res.status(400).json({
+                msg: "User not found"
+            })
+        } else {
+
+            // TEST IF YOU CAN SAVE THIS NEWLY UPDATED POST FOR USER OPTION FOR MUSIC CREATION REQUEST
+            // TODO: ADD MORE FIELDS AND CHANGE THIS FILE TO MIDI.JS OR OTHER WAY AROUND. NEED TO CHECK WITH NOAH ABOUT MORE PARAMTERS NEEDED
+
+            // Create a single or multiple records
+            const newPost = await prisma.user.update({
+                where: { id },
+                data: {
+                    posts: {
+                        create: 
+                            {
+                                title,
+                                // midi,
+                                data: '\TVRoZAAAAAYAAQADAGRNVHJrAAAAGgD/AwtMaXR0bGUgTGFtZQD/UQMKLCsA/y8ATVRyawAAAPMA/wMG\
+                                        THlyaWNzAP8BGEBUTWFyeSBXYXMgQSBMaXR0bGUgTGFtZWT/AQNcTWFL/wEDcnkgGf8BBHdhcyAy/wEC\
+                                        YSAy/wEDbGl0Mv8BBHRsZSAy/wEFbGFtZSxk/wEEL0xpdDL/AQR0bGUgMv8BBWxhbWUsZP8BBC9MaXQy\
+                                        /wEEdGxlIDL/AQVsYW1lLGT/AQMvTWFL/wEDcnkgGf8BBHdhcyAy/wECYSAy/wEDbGl0Mv8BBHRsZSAy\
+                                        /wEFbGFtZSwy/wEDL0EgMv8BA2xpdDL/AQR0bGUgMv8BBWxhbWUgMv8BBHdhcyAy/wEEc2hlIQD/LwBN\
+                                        VHJrAAAA8gD/AwVNdXNpYwDAC2SQQH9LgEBAAJA+fxmAPkAAkDx/MoA8QACQPn8ygD5AAJBAfzKAQEAA\
+                                        kEB/MoBAQACQQH9agEBACpA+fzKAPkAAkD5/MoA+QACQPn9agD5ACpBAfzKAQEAAkEN/MoBDQACQQ39a\
+                                        gENACpBAf0uAQEAAkD5/GYA+QACQPH8ygDxAAJA+fzKAPkAAkEB/MoBAQACQQH8ygEBAAJBAfzKAQEAZ\
+                                        kEB/GYBAQACQPn8ygD5AAJA+fzKAPkAAkEB/MoBAQACQPn8ygD5AAJA8f2RAZABDZABIf1qAPEAAQEAA\
+                                        Q0AASEAK/y8A', // raw midi data for midi file
+                                bpm,
+                                key,
+                            }      
+                    }
+                }, 
+                include: {
+                    posts: true
+                }
+            });
+            res.json(newPost)
+        }
+    } catch(err) {
+        res.status(500).send({msg: err})
+    }
+
+});
+
+//Get user post information by username
+router.get('/findUserPosts', async (req, res) => {
+
+    try {
+        const userPosts = await prisma.user.findUnique({
+            where: { username: req.body.username  },
+            select: {
+                posts: true
+            }
+        });
+
+        if(!userPosts) {
+            return res.status(400).json({
+                msg: "Username not found"
+            })
+        }
+        
+        res.json(userPosts)
+    } 
+    catch(err) {
+        res.status(500).send({msg: err})
+    }
+
+})
+
+// Get user by username
+router.get('/findUser', async (req, res) => {
+
+    try 
+    {
+        const findUser =  await prisma.user.findUnique({
+            where: { username: req.body.username },
+            select: {
+                firstName: true,
+                lastName: true,
+                dob: true,
+                email: true,
+                username: true,
+                bio: true,
+                posts: true
+            }
+        });
+
+        if(!findUser) {
+            return res.status(400).json({
+                msg: "Username does not exist"
+            })
+        }
+        res.json(findUser)
+    } 
+    catch(err) {
+        res.status(500).send({msg: err})
+    }
+
+});
+
+//Update user info 
+router.put('/updateUser', async (req, res) => {
+
+    try 
+    {
+        const {id, firstName, lastName, dob, email, username, bio} = req.body
+        const updateUser = await prisma.user.update({
+            where: { id },
+            data: {
+                    firstName: firstName,
+                    lastName: lastName,
+                    dob: new Date(dob),
+                    email: email,
+                    username: username,
+                    bio,
+            }
+          })
+        //   res.status(200).send({msg: "Updated OK"});
+          res.json(updateUser);
+    } 
+    catch(err) {
+        res.status(500).send(err);
+    }
+    
+});
+
+// Delete user by ID
+router.delete('/deleteUser', async (req, res) => {
+
+    try 
+    { 
+        const deleteUser = await prisma.user.delete({
+            where: { id: req.body.id }
+        })
+        res.status(200).send({msg: "Deleted OK"});
+    } 
+    catch(err) {
+        res.status(500).send(err);
+    }
+    
+});
+
+const transporter = nodemailer.createTransport({
+    port: 465,               // true for 465, false for other ports
+    host: "smtp.gmail.com",
+       auth: {
+            user: 'brainbeatsdev@gmail.com',
+            pass: process.env.EMAIL_PASSWORD,
+         },
+    secure: true,
+});
+
+// Send Email to user to verify login
+router.post('/sendVerificationEmail', async (req, res) => {
+    try 
+    {
+        const { email, subject, text } = req.body;
+        const userExists = await prisma.user.findUnique({
+        where: { email },
+            select: { email: true }
+        });
+
+        // If the user is an existing user, then send a verification email based on their ID
+        if (userExists) {
+            const mailData = {
+                from: 'brainbeatsdev@gmail.com',  // Sender address
+                to: email,                           // List of receivers
+                subject: subject,
+                text: 'Verify your login to BrainBeats by clicking the following link, or copy and paste it into your browser: ',
+                html: '<a href=\"https://www.brainbeats.dev/verify/${userExists._id}\">Verify Email</a>',
+            };
+        
+            transporter.sendMail(mailData, function (err, info) {
+                if(err) {
+                    return console.log(err);
+                } else {
+                    console.log('Email Sent: ' + info.response);
+                }
+        
+                res.status(200).send({message: "Mail send", message_id: info.messageId });
+            });
+        }
+    } 
+    catch(err) {
+        console.log(err)
+        res.status(500).json({msg: "User does not exist."})
+    }
+});
+
+router.post('/jwtStuff', async (req, res) => {
+    try {
+
+
+    } 
+    catch(err) {
+        console.log(err)
+        res.status(500).json({msg: "Unable to create JWT token"})
+    }
+})
+
+// login an existing user
+router.post('/loginUser', async (req, res) => {
+  
+    try {
+
+        // Get user input
+        const { email, password } = req.body;
+    
+        // Validate if user exist in our database
+        const user = await prisma.user.findUnique({
+          where: { email: email }
+          });
+        
+        // if (jwtAPI.verifyJWT()) {
+        //   console.log("User logged in via JWT");
+        //   return res.status(200).json(user, {
+        //       msg: "User authenticated via JWT."
+        //   });
+        // }
+
+        // If password is related to the email console log a successful login
+        if (user && (await bcrypt.compare(password, user.password))) {
+          // res.json("Logged In!")
+          res.json(user)
+          // jwtAPI.giveLoginJWT({id: user.id, email: user.email})
+        } else {
+          return res.status(400).send("Invalid Credentials");
+        }
+      } catch (err) {
+        console.log(err)
+      }
+
+});
+
+//Get user midi information by ID
+router.get('/findMidi', async (req, res) => {
+
+    const userExist = await prisma.user.findUnique({
+        where: { id: userId  },
+    });
+
+    if(!userExist) {
+        return res.status(400).json({
+            msg: "User not found"
+        })
+    } else {
+        const posts = await prisma.post.findMany({
+            where: { id: req.body.id }, 
+            select: {
+                data: true,
+                midi: true
+            }
+        });
+    res.json(posts);
+    }
+})
+
+module.exports = router;
