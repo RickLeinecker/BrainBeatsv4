c98970836790458f55badc9e01e9d020ce9e04ee
Quanminh Nguyen <quanminh@Quanminhs-Air.attlocal.net>
2022-07-27 17:40:34 -0400

    carousel now link to register and about us

--- frontend/src/Components/Carousel/Carousel.js
+++ frontend/src/Components/Carousel/Carousel.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import {Background1} from "../Home/Background1.png";
-import {Background2} from "../Home/Background2.png";
+import Background1 from "./Background1.png";
+import Background2 from "./Background2.png";
+
+import Link from 'react-router-dom'
 
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
@@ -10,10 +12,10 @@ export default () => (
         <Carousel autoPlay showThumbs={false}
         infiniteLoop={true} dynamicHeight={true} interval={10000}>
             <div>
-                <img alt="About Us" src={Background1} />
+                <a href="/About" style={{display: 'block'}}><img alt="About Us" src={Background1} /></a>
             </div>
             <div>
-                <img alt="Create an account" src={Background2} />
+                <a href="/Register" style={{display: 'block'}}><img alt="Create an account" src={Background2} /></a>
             </div>
         </Carousel>
     </div>
