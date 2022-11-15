5c1f15aef7f19915647a3fd9b6b4666e32ef56d2
brandonmrgich <58794109+brandonmrgich@users.noreply.github.com>
2022-11-14 22:04:42 -0500

    fixed some colors, began carousel logic (! Incomplete)

--- frontend/src/Components/Carousel/Carousel.tsx
+++ frontend/src/Components/Carousel/Carousel.tsx
@@ -1,12 +1,41 @@
 import React, { useState } from "react";
+import parse from 'html-react-parser';
+import { Carousel as Caro } from "react-responsive-carousel";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Img1 from '../../images/CarouselImages/XXX.png'
 
 const Carousel = () => {
+    const tempImage = "https://cdn.discordapp.com/attachments/1019947812592562217/1021486319425294396/unknown.png" // "https://img.freepik.com/free-vector/music-studio-control-room-singer-booth_107791-1637.jpg?w=2000";
+    const imageArray = [tempImage];
 
-    const[isMoving, setIsMoving] = useState(false);
-    const toggle = () => setIsMoving(!isMoving);
-    return (<div>
+    const spawnImages = () => {
+        let imageDivs = [""];
+         for (let i = 0; i < imageArray.length; i++) {
+            imageDivs.push(
+                '<div id="cardContainer">' + 
+                    '<img alt="About Us" src=' + imageArray[i] + '/>' + 
+                    '<Button id="cardStyle" href="/About">About Us</Button>' + 
+                '</div>'
+            );
+         }
+         return imageDivs;
+    }
 
-    </div>)
+    let images = spawnImages();
+
+    return (
+    <div className="container">
+        <Caro autoPlay showThumbs={false} infiniteLoop={true} 
+            dynamicHeight={false} interval={10000}
+        > 
+        {images.map((image) => (
+            <div>{parse(image)}</div>
+        ))}
+
+        </Caro>
+    </div>
+    )
 }
 
 export default Carousel;
--- frontend/src/Components/Home/Home.tsx
+++ frontend/src/Components/Home/Home.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react';
 // import {userJWT, userModeState} from '../context/GlobalState'
 import './Home.css';
 import TrackCard from '../TrackCard/TrackCard';
+import Carousel from '../Carousel/Carousel';
 
 const Home = () => {
 
@@ -9,10 +10,14 @@ const Home = () => {
     
     return (
         <div className='container' id='main-container'>
-            <img src="https://img.freepik.com/free-vector/music-studio-control-room-singer-booth_107791-1637.jpg?w=2000" className="img-fluid" id="home-image" alt="..." />
+            <Carousel />
+            {/* <img src="https://img.freepik.com/free-vector/music-studio-control-room-singer-booth_107791-1637.jpg?w=2000" className="img-fluid" id="home-image" alt="..." /> */}
             <br /><br />
-            <h3 className="text-decoration-underline">Featured Tracks</h3>
-            <TrackCard />
+            <h3 className="text-decoration-underline" id="featured-tracks-heading">Featured Tracks</h3>
+            <div className='container' id='track-card-container'>
+                <TrackCard />
+            </div>
+            
         </div>);
 
 
