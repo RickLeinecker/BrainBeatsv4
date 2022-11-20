5e8269c176169638df2ce38d7ae8b9f5b0c18adc
Martin McCarthy <martinmccarthy@knights.ucf.edu>
2022-11-20 09:23:13 -0500

    adds style to login forms

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react";
+import './Login.css';
 
 const Login = () => {
     const [username, setUsername] = useState('');
--- frontend/src/Components/SignUp/SignUp.tsx
+++ frontend/src/Components/SignUp/SignUp.tsx
@@ -1,14 +1,17 @@
 import React, {useEffect, useState} from "react";
+import './SignUp.css';
 
 const SignUp = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [fullName, setFullName] = useState('');
     const [email, setEmail] = useState('');
+    const [errorMsg, setErrorMsg] = useState('');
 
     function validateEmail(inputEmail:string) {
-        var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        var emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-])*$/;
         if (inputEmail.match(emailRegex)) return true;
+        setErrorMsg('Invalid email input, please re-enter and try again.');
         return false;
     }
     
@@ -18,7 +21,7 @@ const SignUp = () => {
             "email": email,
             "username": username,
             "password": password
-        }
+        };
 
         if(validateEmail(email)) {
             // continue register
@@ -27,26 +30,27 @@ const SignUp = () => {
 
     return (
         <div className='container' id='main-container'>
-            <h1 className="text-center fw-semibold">Welcome to BrainBeats!</h1>
+            <h1 className="text-center fw-semibold signup-text">Welcome to BrainBeats!</h1>
             <div className="mb-3">
-                <label className="form-label form-text">Full Name</label>
+                <label className="form-label signup-text">Full Name</label>
                 <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Full Name" onChange={event => setFullName(event.target.value)}/>
             </div>
             <div className="mb-3">
-                <label className="form-label form-text">Email</label>
+                <label className="form-label signup-text">Email</label>
                 <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Email" onChange={event => setEmail(event.target.value)}/>
             </div>
             <div className="mb-3">
-                <label className="form-label form-text">Username</label>
+                <label className="form-label signup-text">Username</label>
                 <input type="text" className="form-control" id="formGroupExampleInput" placeholder="Username" onChange={event => setUsername(event.target.value)}/>
             </div>
             <div className="mb-3">
-                <label className="form-label form-text">Password</label>
+                <label className="form-label signup-text">Password</label>
                 <input type="password" className="form-control" id="formGroupExampleInput2" placeholder="Password" onChange={event => setPassword(event.target.value)}/>
             </div>
             <div className='container text-center'>
                 <button type="submit" className="btn btn-primary" onClick={doSignUp}>Sign up</button>
             </div>
+            <span className="text-center error-msg">{errorMsg}</span>
         </div>);
 }
 
